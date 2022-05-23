30fd943f83c7310b5de3fe446d244859cc272a58
Ana Canto <ana.lucia.canto@poli.ufrj.br>
2022-05-23 15:49:26 -0300

    Adding support libraries for graphs.

--- TP2/src/adder/index.cpp
+++ TP2/src/adder/index.cpp
@@ -7,21 +7,14 @@
 #include <atomic>
 #include "./utils.cpp"
 #include <cstring>
+#include "gnuplot-iostream.h"
+#include "./supportLib.cpp"
+#include "./pbPlots.cpp"
 
 using namespace std;
 
-//Mudar N de acordo com os casos de teste.
-//Testar com os casos N = 10^7, N = 10^8 e N = 10^9.
-
-#define N 10
-// K = número de threads que vão trabalhar simultaneamente
-#define K 2
-
-
-int totalSum = 0;    //Armazena  a soma total.
+int totalSum = 0;    //Acumulador compartilhado
 atomic_flag lock_stream = ATOMIC_FLAG_INIT;  
-
-
 
 void acquire(atomic_flag *lock)
 {
@@ -46,27 +39,23 @@ vector<int> populateVector(int size)
 
 void sumVector(vector<int> vectorToSum, int numThreads ){
     int sum = accumulate(vectorToSum.begin(), vectorToSum.end(), 0);  // Somando o vetor
-    acquire(&lock_stream);      //Bloquear a área crítica
-    totalSum += sum;            //Acessa o endereço de memória compartilhado.
-    release(&lock_stream);      //Libera a área crítica
+    acquire(&lock_stream);  
+    //Região crítica  
+    totalSum += sum;          
+    release(&lock_stream);     
 }
 
 
-int main(){
+float adderWithSpinlocks(vector<int>  numbersVector, int K, int N) {
 
-    int numberOfExecutions = 2;  //Quantas
-    double totalTime = 0;
+        double totalTime = 0;
 
-
-    int numberOfThreads = K;
-    vector<int> numbers = populateVector(N);
-
-     for (int i = 1; i <= numberOfExecutions; i++)    {
 
         int vectorSize = N / K;   //Utilizando a sugestão do professor de dividir  N/K
 
-        vector<vector<int>> vectorOfChunks = splitVectorIntoChunks(numbers, vectorSize);
-        //retorna vários vetores
+
+        //Dividindo o vetor em subvetores
+        vector<vector<int>> vectorOfChunks = splitVectorIntoChunks(numbersVector, vectorSize);
         
         vector<thread> v;
 
@@ -82,24 +71,71 @@ int main(){
 
         for (int i = 0; i < K; i++)
         {
-            // Must wait for each thread to finish
             v[i].join();
         }
 
-        // Ending to measure time
+        // Finalizando a contagem do tempo
         auto end = chrono::system_clock::now();
 
         chrono::duration<double> elapsed = end - start;
 
-        totalTime = totalTime + elapsed.count();
-        printf("Número de threads %i\n" , K);
-        printf("Número de elementos no vetor inicial %i\n" , N);
-        printf("Número de elementos no vetor dividido %i\n" , vectorSize);
-        printf("Soma acumulada %i\n", totalSum);          
-        printf("Total time: %f\n", totalTime );
-                        
+        totalTime = totalTime + elapsed.count();    
+
+        return totalTime;  
+
+}
+
+float execute(int K, int N){
+
+    int numberOfExecutions = 10;  
+    double totalTimeForNumberOfExecutions = 0;
+    vector<int> numbersVector = populateVector(N); //Populando o vetor de tamanho N com números no intervalo [-100,100] 
+
+     for (int i = 1; i <= numberOfExecutions; i++)    {
+         totalTimeForNumberOfExecutions+= adderWithSpinlocks(numbersVector, K, N);
 
      }
+    printf("-----------------------------------------------------");        
+    printf("Número de threads %i\n" , K);
+    printf("Número de elementos no vetor inicial %i\n" , N);
+    printf("Soma total %i\n" , totalSum);
+    printf("Tempo médio para 10 iterações %f\n", totalTimeForNumberOfExecutions/numberOfExecutions);        
+    printf("------------------------------------------------------");        
+
      
+    return  totalTimeForNumberOfExecutions/numberOfExecutions;
+
+}
+
+
+int main(){
+   
+    vector<double> totalTime;
+    vector<double> nThreads;
+
+    for (int n = 10000000; n <=1000000000;){
+        for (int k = 1; k <=  1;){
+            
+            totalTime.push_back(execute(k, n));
+            nThreads.push_back(k);
+            k = k*2;
+        }
+        n = n*10; 
+    }
+    
+    RGBABitmapImageReference *imageReference = CreateRGBABitmapImageReference();
+    
+    StringReference *errorMessage = new StringReference();
+    
+    bool success = DrawScatterPlot(imageReference, 600, 400, &totalTime, &nThreads, errorMessage);
+
+    if(success){
+        vector<double> *pngdata = ConvertToPNG(imageReference->image);
+        WriteToFile(pngdata, "example1.png");
+        DeleteImage(imageReference->image);
+	}else{
+	   printf("Houve erro ao gerar gráfico");
+	}
+
     return 0;
 }
